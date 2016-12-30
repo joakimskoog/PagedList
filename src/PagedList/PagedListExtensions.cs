cc29e4908b2f1be076f8518faad6f27ffb838555
joakimskoog <joakimskooog@gmail.com>
2016-12-30 18:06:42 +0100

    Added extensions for the interface and matching tests

---
+++ src/PagedList/PagedListExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace PagedList
+{
+    public static class PagedListExtensions
+    {
+        /// <summary>
+        /// Checks whether or not this is the first page in the superset.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pagedList"></param>
+        /// <returns>True if this is the first page in the superset, false if not.</returns>
+        public static bool IsFirstPage<T>(this IPagedList<T> pagedList)
+        {
+            return pagedList.PageNumber == pagedList.FirstPageNumber;
+        }
+
+        /// <summary>
+        /// Checks whether or not this is the last page in the superset.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pagedList"></param>
+        /// <returns>True if this is the last page in the superset, false if not.</returns>
+        public static bool IsLastPage<T>(this IPagedList<T> pagedList)
+        {
+            return pagedList.PageNumber == pagedList.LastPageNumber;
+        }
+
+        /// <summary>
+        /// Checks whether or not there is another page after this one in the superset.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pagedList"></param>
+        /// <returns>True if there is another page after this one in the superset, false if not.</returns>
+        public static bool HasNextPage<T>(this IPagedList<T> pagedList)
+        {
+            return pagedList.NextPageNumber.HasValue;
+        }
+
+        /// <summary>
+        /// Checks whether or not there is a previous page before this one in the superset.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pagedList"></param>
+        /// <returns>True if there is a previous page before this one in the superset, false if not.</returns>
+        public static bool HasPreviousPage<T>(this IPagedList<T> pagedList)
+        {
+            return pagedList.PreviousPageNumber.HasValue;
+        }
+    }
+}
---
+++ tests/PagedList.Tests/PagedListExtensionsTests.cs
@@ -0,0 +1,90 @@
+﻿using Moq;
+using Xunit;
+
+namespace PagedList.Tests
+{
+    public class PagedListExtensionsTests
+    {
+        [Fact]
+        public void OnlyOnePageInSuperset_ThenThatPageIsFirstPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(1);
+            pagedList.SetupGet(list => list.FirstPageNumber).Returns(1);
+
+            Assert.True(pagedList.Object.IsFirstPage());
+        }
+
+        [Fact]
+        public void LastPageOfTwo_ThenThatPageIsNotFirstPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(2);
+            pagedList.SetupGet(list => list.FirstPageNumber).Returns(1);
+
+            Assert.False(pagedList.Object.IsFirstPage());
+        }
+
+        [Fact]
+        public void OnlyOnePageInSuperset_ThenThatPageIsLastPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(1);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(1);
+
+            Assert.True(pagedList.Object.IsLastPage());
+        }
+
+        [Fact]
+        public void FirstPageOfTwo_ThenThatPageIsNotLastPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(1);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(2);
+
+            Assert.False(pagedList.Object.IsLastPage());
+        }
+
+        [Fact]
+        public void FirstPageOfTwo_ThenThatPageHasNextPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(1);
+            pagedList.SetupGet(list => list.NextPageNumber).Returns(2);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(2);
+
+            Assert.True(pagedList.Object.HasNextPage());
+        }
+
+        [Fact]
+        public void LastPageOfTwo_ThenThatPageHasNotANextPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(2);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(2);
+
+            Assert.False(pagedList.Object.HasNextPage());
+        }
+
+        [Fact]
+        public void LastPageOfTwo_ThenThatPageHasPreviousPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(2);
+            pagedList.SetupGet(list => list.PreviousPageNumber).Returns(1);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(2);
+
+            Assert.True(pagedList.Object.HasPreviousPage());
+        }
+
+        [Fact]
+        public void FirstPageOfTwo_ThenThatPageHasNotAPreviousPage()
+        {
+            var pagedList = new Mock<IPagedList<string>>();
+            pagedList.SetupGet(list => list.PageNumber).Returns(1);
+            pagedList.SetupGet(list => list.LastPageNumber).Returns(2);
+
+            Assert.False(pagedList.Object.HasPreviousPage());
+        }
+    }
+}
