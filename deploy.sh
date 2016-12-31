b7aa7a8f17889bc189e4c355783e7b776b498fb5
Joakim Skoog <joakimskooog@gmail.com>
2016-12-31 13:46:18 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 echo $NUGET_API_KEY
 echo {$NUGET_API_KEY}
+echo ${$NUGET_API_KEY}
 dotnet pack src/*/ --output .artifacts --no-build --configuration Release 
 mono .nuget/nuget.exe push .artifacts/*.nupkg $NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
